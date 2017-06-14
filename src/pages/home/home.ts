bb0bfe765941d31f66a7a77ecdf0940fe2e9b0c7
Berke Muhtaroglu <berkeebay@gmail.com>
2017-06-14 11:48:21 +0300

    added page 2

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { Page2Page } from '../page2/page2';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -9,6 +11,11 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  nextPage() {
+    this.navCtrl.push(Page2Page)
+    console.log("oh!");
   }
 
 }
